7b6a435653732dd69cf8743a0d4f059f98960376
kkuczniak <51820616+kkuczniak@users.noreply.github.com>
2021-08-06 17:00:39 +0200

    start

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '65bd5ebf927b71b27eed45d7e33be660'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c2e2c18b-4bea-4109-a336-0cf94663ac84'
+};
